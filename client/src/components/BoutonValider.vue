ec74387ddf8c99c723f1f7e8f6e030d856a47442
Aline <alinefurette@gmail.com>
2024-03-12 17:05:08 +0100

    first commit

---
+++ client/src/components/BoutonValider.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <input type="submit">
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ client/src/views/Error.vue
@@ -0,0 +1,48 @@
+<script setup>
+    // import BoutonError from '@/components/BoutonError.vue'
+</script>
+
+<template>
+
+    <div class="texte"><h1>Cette page n'existe pas</h1></div>
+
+    <div class="tomate-erreur">
+        <img class="tomato" src="../img/Errorpage.jpg" alt="Page erreur">
+    </div>
+
+    <button class="btn">Nouvelle saisie</button>
+
+
+</template>
+
+<style scoped>
+
+
+
+.texte {
+        display: flex;
+        flex-wrap: column nowrap;
+        justify-content: center;
+        align-content: center;
+        color: var(--green);
+}
+
+.tomate-erreur {
+        display: flex;
+        flex-wrap: row nowrap;
+        justify-content: center;
+        align-content: center;
+    }
+
+    .tomato {
+        width: 75%;
+    }
+
+    .btn {
+        width: 100px;
+        height: 30px;
+        border-radius: 10px;
+        background-color: var(--button-active);
+        color: beige;
+    }
+</style>

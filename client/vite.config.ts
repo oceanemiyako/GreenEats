33830abf09a168773007ab9840a0272ff8132378
Rosiere_Mathieu <rosiere.mathieu@gmail.com>
2024-03-13 10:04:20 +0100

    deploying app via github

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,46 +1,46 @@
 import { fileURLToPath, URL } from "node:url";
 import { VitePWA } from "vite-plugin-pwa";
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/GreenEats/",
   plugins: [
-    basicSsl(),
     vue(),
     VitePWA({
       registerType: "autoUpdate",
-      injectRegister: 'auto',
+      injectRegister: "auto",
       workbox: {
         clientsClaim: true,
         skipWaiting: true,
       },
       devOptions: {
-        enabled: true
+        enabled: true,
       },
       manifest: {
-        name: "myapp",
+        name: "GreenEats",
         short_name: "app",
         icons: [
           {
-              src: "/android-chrome-192x192.png",
-              sizes: "192x192",
-              type: "image/png"
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-              src: "/android-chrome-512x512.png",
-              sizes: "512x512",
-              type: "image/png"
-          }
-      ],
-      theme_color: "#ffffff",
-      background_color: "#ffffff",
-      display: "standalone"
-      }
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
     }),
   ],
+
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),

b556ed5c4435a5f2c97b2e4577f979d5be4ad40d
Oceane <garciaoceane3122@gmail.com>
2024-05-02 14:43:37 +0200

    debutback

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ app.get("/", (req , res) => {
     res.json({message: "Bienvenue sur notre API"});
 } );
 
+const users = require("./routes/user");
+app.use("/user", users);
+
 app.listen(port, (err) => {
     console.log("Serveur est en ligne ! ");
 } )

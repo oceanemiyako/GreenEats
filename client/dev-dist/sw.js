1d09e8a9de80f63f7cb573f887594979acf72d92
Oceane <garciaoceane3122@gmail.com>
2024-05-03 11:34:17 +0200

    connectionBDD

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "1ddc89fccf352b1615e259539eaabf89"
   }, {
     "url": "index.html",
-    "revision": "0.hbo633keag"
+    "revision": "0.pnchrha1fjg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const port = process.env.PORT || 5000; 
 
+
+
 const app = express();
 
 app.get("/", (req , res) => {
@@ -10,6 +12,24 @@ app.get("/", (req , res) => {
 
 const users = require("./routes/user");
 app.use("/user", users);
+
+
+
+const connection = require('./config/db');
+
+app.post('/api/ajouter', (req, res) => {
+    const { nom, email , statut } = req.body;
+    connection.query('INSERT INTO utilisateurs (nom, email,statut) VALUES (?, ?)', [nom, email, statut], (err, result) => {
+        if (err) throw err;
+        res.send('Utilisateur ajouté avec succès');
+    });
+});
+
+
+
+
+
+
 
 app.listen(port, (err) => {
     console.log("Serveur est en ligne ! ");
